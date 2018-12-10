a32df6b53df24456fcb90e35476708d8805edeb3
Rafael Hernandez <rhernanf@hotmail.com>
2018-12-10 09:44:29 -0600

    Carga Inicial CENTRAL

---
+++ grails-app/services/CENTRAL/SqtPasswordService.groovy
@@ -0,0 +1,18 @@
+package CENTRAL
+
+import grails.gorm.services.Service
+
+@Service(SqtPassword)
+interface SqtPasswordService {
+
+    SqtPassword get(Serializable id)
+
+    List<SqtPassword> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    SqtPassword save(SqtPassword sqtPassword)
+
+}
---
+++ grails-app/services/CENTRAL/SqtVentaAcumuladaProductivaService.groovy
@@ -0,0 +1,18 @@
+package CENTRAL
+
+import grails.gorm.services.Service
+
+@Service(SqtVentaAcumuladaProductiva)
+interface SqtVentaAcumuladaProductivaService {
+
+    SqtVentaAcumuladaProductiva get(Serializable id)
+
+    List<SqtVentaAcumuladaProductiva> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    SqtVentaAcumuladaProductiva save(SqtVentaAcumuladaProductiva sqtVentaAcumuladaProductiva)
+
+}
